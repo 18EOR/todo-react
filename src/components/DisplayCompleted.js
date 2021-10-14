0c4529e3a3a4e60bd5fe5ceb2defede1a409af6c
18EOR <42158152+18EOR@users.noreply.github.com>
2021-10-14 13:49:45 +0100

    Added hover titles to buttons

--- src/components/DisplayCompleted.js
+++ src/components/DisplayCompleted.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function DisplayCompleted(props) {
-  const {todo, returnToActive, removeCompleted} = props
+  const { todo, returnToActive, removeCompleted } = props;
 
   return (
     <div className="todo-display">
       <div>{todo.text}</div>
       <div className="comp-display-end">
         <div className="comp-date">{todo.compTime}</div>
-        <div onClick={() => returnToActive(todo)} className="btn-blue"><i className="fas fa-undo-alt"></i></div>
-        <div className="btn-red" onClick={() => removeCompleted(todo.id)}><i className="fas fa-times-circle"></i></div>
+        <div
+          title="Restore"
+          onClick={() => returnToActive(todo)}
+          className="btn-blue"
+        >
+          <i className="fas fa-undo-alt"></i>
+        </div>
+        <div
+          title="Delete"
+          className="btn-red"
+          onClick={() => removeCompleted(todo.id)}
+        >
+          <i className="fas fa-times-circle"></i>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default DisplayCompleted
+export default DisplayCompleted;
--- src/components/TodoDisplay.js
+++ src/components/TodoDisplay.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function TodoDisplay(props) {
-  const {todo, remove, addToCompleted} = props
+  const { todo, remove, addToCompleted } = props;
 
   return (
     <div className="todo-display">
       <div className="todo-text">{todo.text}</div>
       <div>
-        <div className="btn-green" onClick={() => addToCompleted(todo)}><i className="fas fa-check-circle"></i></div>
-        <div className="btn-red" onClick={() => remove(todo.id)}><i className="fas fa-times-circle"></i></div>
+        <div
+          title="Completed"
+          className="btn-green"
+          onClick={() => addToCompleted(todo)}
+        >
+          <i className="fas fa-check-circle"></i>
+        </div>
+        <div title="Delete" className="btn-red" onClick={() => remove(todo.id)}>
+          <i className="fas fa-times-circle"></i>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TodoDisplay
+export default TodoDisplay;
